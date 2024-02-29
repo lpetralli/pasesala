fb3087b92de263668f13e1b8128d43d29fde36d0
lpetralli <lucas.petralli@webee.io>
2024-02-29 15:39:13 -0300

    tls

--- pages/1_Nuevo_Paciente.py
+++ pages/1_Nuevo_Paciente.py
@@ -10,7 +10,7 @@ from pymongo.server_api import ServerApi
 uri = "mongodb+srv://lucaspetralliadf:4cgH0uxNSnfOGtr3@pruebahua.olbttjo.mongodb.net/?retryWrites=true&w=majority&appName=pruebaHUA"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, tls=True, server_api=ServerApi('1'))
 db = client['Hospital']
     
 image = Image.open('Hospital_Austral_Logo_2023.png')
